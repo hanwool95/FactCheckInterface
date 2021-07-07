a28a6a0a5f3013745a2e7de4dfc4236d5ab90ff4
hanwool <hanwool95@snu.ac.kr>
2021-07-07 15:15:18 +0900

    make home login

--- FactcheckInterface/FactcheckInterface/urls.py
+++ FactcheckInterface/FactcheckInterface/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('common/', include('common.urls')),
     path('survey/', include('survey.urls')),
+    path('', auth_views.LoginView.as_view(template_name='common/login.html'), name='login')
 ]
