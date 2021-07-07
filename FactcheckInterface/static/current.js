ac1561f6ce1af8ba27243e5c8e325f550659ca58
hanwool <hanwool95@snu.ac.kr>
2021-07-07 18:51:56 +0900

    update pagination up to 10

--- FactcheckInterface/static/current.js
+++ FactcheckInterface/static/current.js
@@ -1,76 +1,131 @@
 // 출처: https://codepen.io/blake_green/pen/VyXXQY
 
-function pagination(){
-        var req_num_row=10;
-        var $tr=jQuery('tbody tr');
-        var total_num_row=$tr.length;
-        var num_pages=0;
-        if(total_num_row % req_num_row ==0){
-            num_pages=total_num_row / req_num_row;
-        }
-        if(total_num_row % req_num_row >=1){
-            num_pages=total_num_row / req_num_row;
-            num_pages++;
-            num_pages=Math.floor(num_pages++);
-        }
-  
-    //jQuery('.pagination').append("<li><a class=\"prev\">Previous</a></li>");
+var global_state = 0;
+var req_num_row=10;
+var count = 0;
 
-        for(var i=1; i<=num_pages; i++){
-            jQuery('.pagination').append("<li>&nbsp;<a>"+i+"</a></li>");
-      jQuery('.pagination li:nth-child(2)').addClass("active");
-      jQuery('.pagination a').addClass("pagination-link");
+
+function make_peg(num_pages){
+    // 이전 단추가 첫 페이지에서 생성 안되게 만듦.
+    jQuery('.pagination').append("<li><a class=\"begin\"><b>처음으로</b></a></li>");
+
+    if(global_state != 0){
+        jQuery('.pagination').append("<li><a class=\"prev\">이전</a></li>");
+      }
+
+    for(var i=1; i<=num_pages; i++){
+        var state = global_state + i
+        if(i === 1){
+            jQuery('.pagination').append("<li class='active' id='first'>&nbsp;<a>"+state+"</a></li>");
         }
+        else{
+            jQuery('.pagination').append("<li>&nbsp;<a>"+state+"</a></li>");
+        }
+        jQuery('.pagination a').addClass("pagination-link");
+        }
+
+    if(global_state != parseInt(count / 100)*10) {
+        jQuery('.pagination').append("<li><a class=\"next\">다음</a></li>");
+    }
+    jQuery('.pagination').append("<li><a class=\"last\"><b>끝으로</b></a></li>");
+
+
+}
+
+function show_table(start){
+    jQuery('tbody tr').hide();
+    for(var i=0; i< req_num_row; i++){
+        jQuery('tbody tr').eq(+start+i).show();}
+}
+
+function pagination(){
+
+    var $tr=jQuery('tbody tr');     // 표의 tbody를 불러옴
+    var total_num_row=$tr.length - (global_state*10);   // tbody 길이 불러옴
+    count = $tr.length
+
+    // page 개수 계산
+    var num_pages=0;
+    if(total_num_row % req_num_row ==0){
+        num_pages=total_num_row / req_num_row;
+    }
+    if(total_num_row % req_num_row >=1){
+        num_pages=total_num_row / req_num_row;
+        num_pages++;
+        num_pages=Math.floor(num_pages++);
+    }
+
+    if (num_pages >= 11){
+        num_pages = 10;
+    }
+
+    make_peg(num_pages)
   
-    //jQuery('.pagination').append("<li><a class=\"next\">Next</a></li>");
+    $tr.each(function(i){
+        jQuery(this).hide();
+        if(i+1 <= req_num_row){
+            $tr.eq(i).show();
+        }
+    });
   
-        $tr.each(function(i){
-      jQuery(this).hide();
-      if(i+1 <= req_num_row){
-                $tr.eq(i).show();
-            }
-        });
-  
-        jQuery('.pagination a').click('.pagination-link', function(e){
-            e.preventDefault();
-            $tr.hide();
-            var page=jQuery(this).text();
-            var temp=page-1;
-            var start=temp*req_num_row;
-      var current_link = temp;
-      
-      jQuery('.pagination li').removeClass("active");
-            jQuery(this).parent().addClass("active");
-    
-            for(var i=0; i< req_num_row; i++){
-                $tr.eq(start+i).show();
-            }
-      
-      if(temp >= 1){
-        jQuery('.pagination li:first-child').removeClass("disabled");
-      }
-      else {
-        jQuery('.pagination li:first-child').addClass("disabled");
-      }
-            
-        });
-  
-    jQuery('.prev').click(function(e){
+    jQuery('.pagination a').click('.pagination-link', function(e){
         e.preventDefault();
-        jQuery('.pagination li:first-child').removeClass("active");
+        $tr.hide();
+        var page=jQuery(this).text();       // 현재 페이지 수
+        var temp=page-1;
+        var start=temp*req_num_row;
+
+        jQuery('.pagination li').removeClass("active");
+        jQuery(this).parent().addClass("active");
+
+        show_table(start);
+
+
+
+        });
+
+    $("#first").trigger("click");
+
+    jQuery('.begin').click(function(){
+        global_state = 0
+        console.log(global_state);
+        jQuery('.pagination').empty()
+        pagination()
+        show_table(global_state*req_num_row);
+    });
+
+    jQuery('.prev').click(function(){
+        global_state -= 10
+        console.log(global_state);
+        jQuery('.pagination').empty()
+        pagination()
+        show_table(global_state*req_num_row);
     });
  
     jQuery('.next').click(function(e){
-        e.preventDefault();
-        jQuery('.pagination li:last-child').removeClass("active");
+        global_state += 10
+        console.log(global_state);
+        jQuery('.pagination').empty();
+        pagination();
+        show_table(global_state*req_num_row);
+
+    });
+
+    jQuery('.last').click(function(){
+        global_state = parseInt($tr.length / 100)*10
+        console.log(global_state);
+        jQuery('.pagination').empty();
+        pagination();
+        show_table(global_state*req_num_row);
     });
  
     }
- 
+
+
+
 jQuery('document').ready(function(){
     pagination();
   
   jQuery('.pagination li:first-child').addClass("disabled");
   
 });
-
